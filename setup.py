a0d1833411b0b3986f3470d8396fbc3ea9b8a231
Alessandro Comodi <acomodi@antmicro.com>
2020-07-06 12:07:02 +0200

    add setup.py
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017-2020  The Project X-Ray Authors.
+#
+# Use of this source code is governed by a ISC-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/ISC
+#
+# SPDX-License-Identifier: ISC
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xc-fasm",
+    version="0.0.1",
+    author="SymbiFlow Authors",
+    author_email="symbiflow@lists.librecores.org",
+    description="XC FASM libraries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SymbiFlow/xc-fasm",
+    packages=['xc_fasm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: ISC License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "prjxray@git+git://github.com/SymbiFlow/prjxray.git#egg=prjxray",
+    ],
+    entry_points={
+        'console_scripts': ['fasm2frames=xc_fasm.fasm2frames:main'],
+    })
